a879dcb1dd3b2a2f6cabe5cd90fa4956b6027bc8
Severino-David <severinod.neto@unifacol.edu.br>
2025-03-26 16:15:22 -0300

    Update Aluno.java

--- src/entities/Aluno.java
+++ src/entities/Aluno.java
@@ -3,14 +3,14 @@ package entities;
 public class Aluno {
     private String matricula;
     private String nome;
-    private String curso;
+    private String modalidade;
 
     // Construtores
     public Aluno() {}
-    public Aluno(String matricula, String nome, String curso) {
+    public Aluno(String matricula, String nome, String modalidade) {
         this.matricula = matricula;
         this.nome = nome;
-        this.curso = curso;
+        this.modalidade = modalidade;
     }
 
     // Getters e Setters
@@ -18,11 +18,11 @@ public class Aluno {
     public void setMatricula(String matricula) { this.matricula = matricula; }
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-    public String getCurso() { return curso; }
-    public void setCurso(String curso) { this.curso = curso; }
+    public String getModalidade() { return modalidade; }
+    public void setModalidade(String modalidade) { this.modalidade = modalidade; }
 
     @Override
     public String toString() {
-        return "Aluno [matricula=" + matricula + ", nome=" + nome + ", curso=" + curso + "]";
+        return "Aluno [matricula=" + matricula + ", nome=" + nome + ", modalidade=" + modalidade + "]";
     }
 }
